2d51e2b38eff47ca1334916305a359267f4fab3a
Pranshu321 <pranshujain0111@gmail.com>
2022-12-27 01:35:12 +0530

    Final code

--- client/main.js
+++ client/main.js
@@ -74,6 +74,30 @@ const handleSubmit = async (e) => {
 	const messageDiv = document.getElementById(uniqueId);
 
 	loader(messageDiv);
+	const response = await fetch("https://codeai-uh8b.onrender.com", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			prompt: data.get("prompt"),
+		}),
+	});
+
+	clearInterval(loadInterval);
+	messageDiv.innerHTML = " ";
+
+	if (response.ok) {
+		const data = await response.json();
+		const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
+
+		typeText(messageDiv, parsedData);
+	} else {
+		const err = await response.text();
+
+		messageDiv.innerHTML = "Something went wrong";
+		alert(err);
+	}
 };
 
 form.addEventListener("submit", handleSubmit);
