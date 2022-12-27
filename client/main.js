3645db7a3368c46353981038b13388c17df1f662
Pranshu321 <pranshujain0111@gmail.com>
2022-12-27 16:42:55 +0530

    Final Play

--- client/main.js
+++ client/main.js
@@ -73,7 +73,7 @@ const handleSubmit = async (e) => {
 
 	const messageDiv = document.getElementById(uniqueId);
 	loader(messageDiv);
-	const response = await fetch("http://localhost:5000", {
+	const response = await fetch("https://codeai-uh8b.onrender.com", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
