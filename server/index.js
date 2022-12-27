012b8ce161251454a46bc80fae66a1ec6f26bd11
Pranshu321 <pranshujain0111@gmail.com>
2022-12-27 16:06:02 +0530

    Api key direct added

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import { Configuration, OpenAIApi } from "openai";
 dotenv.config();
 
 const configuration = new Configuration({
-	apiKey: process.env.OPENAI_API_KEY,
+	apiKey: "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",
 });
 
 const openai = new OpenAIApi(configuration);
